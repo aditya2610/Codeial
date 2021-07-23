684950f4bd8139158ef679dffbf515b1ce8a804c
Aditya Agarwal <adityaagarwal109@gmail.com>
2021-07-23 23:24:55 +0530

    updated

--- public/assets/js/header-5bfef569a2.js
+++
@@ -1,1 +0,0 @@
-window.onscroll=function(){myFunction()};let header=document.getElementById("myHeader"),sticky=header.offsetTop;function myFunction(){window.pageYOffset>sticky?header.classList.add("sticky"):header.classList.remove("sticky")}
