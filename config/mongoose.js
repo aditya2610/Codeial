24479d827f49e01c50f3f5534895494ab567eab4
Aditya Agarwal <adityaagarwal109@gmail.com>
2021-07-24 14:36:34 +0530

    updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`mongodb+srv://aditya26:psCQAJrfVi1f8c4D@stock-talks-cluster.e36bl.mongodb.net/${env.db}?retryWrites=true&w=majority&wtimeout=1000/`);
+//mongoose.connect(`mongodb+srv://aditya26:psCQAJrfVi1f8c4D@stock-talks-cluster.e36bl.mongodb.net/${env.db}?retryWrites=true&w=majority&wtimeout=1000/`);
 
 //psCQAJrfVi1f8c4D
 
- //mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(`mongodb://localhost/${env.db}`);
 
 const db = mongoose.connection;
 
